8152c6aa0c2c4711333c4b560b9de7d1965eacb5
Levon Kirakosyan <noosx.e@gmail.com>
2015-10-12 00:55:56 +0400

    travis-ci enabled redis server, added tests

--- test/test.js
+++ test/test.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const chai = require("chai");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+const expect = chai.expect;
+chai.use(sinonChai);
+
+const HuglaRedisSession = require('./../index.js');
+
+describe("HuglaRedisSession", function() {
+
+  it("should call app#getModule with 'hugla-http' parameter", function() {
+    const app = { config: {} };
+    app.getModule = sinon.spy(function() {
+      return { addMiddlewareSetupAction: function() {} };
+    });
+    const huglaRedisSession = new HuglaRedisSession(app);
+    expect(app.getModule).to.have.been.calledWithExactly('hugla-http');
+  });
+
+  it("should call http#addMiddlewareSetupAction", function() {
+    const spy = sinon.spy();
+    const app = { config: {} };
+    app.getModule = function() {
+      return { addMiddlewareSetupAction: spy };
+    };
+    const huglaRedisSession = new HuglaRedisSession(app);
+    expect(spy).to.have.been.called;
+  });
+
+  describe("#middlewareSetup()", function() {
+    it("should call app#use", function() {
+      const app = { config: {} };
+      app.getModule = function() {
+        return { addMiddlewareSetupAction: function() {} };
+      };
+      const spied = {};
+      spied.use = sinon.spy();
+      const huglaRedisSession = new HuglaRedisSession(app);
+      huglaRedisSession.middlewareSetup(spied);
+      expect(spied.use).to.have.callCount(1);
+    });
+  });
+});
